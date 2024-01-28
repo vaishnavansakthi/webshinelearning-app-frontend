845f44b7aee699d2bd60f5c2f4f2d2ee7e878790
vaishnavanm5 <vaishnavanm5@gmail.com>
2024-01-28 17:24:20 +0530

    fix: updating the code commit flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
 
+/**
+ * Creating App Component
+ * @returns {React.Component} App component
+ */
 function App() {
+  const [greet] = useState('Everyone!')
+
   return (
     <>
-      <h1>Hello Everyone!, Project Yet to start</h1>
+      <h1>Hello {greet}, Project Yet to start</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad excepturi esse distinctio labore obcaecati suscipit
+        repellendus, odio aliquid in voluptates, animi, quae itaque cumque aspernatur provident maxime officiis mollitia
+        maiores!
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
