0ed584b03d34bc9e7f489ce5935671010fffd8c7
vaishnavanm <vaishnavanm5@gmail.com>
2024-01-28 22:53:34 +0530

    test pr template

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,15 @@ function App() {
 
   return (
     <>
-      <h1>Hello {greet}, Project Yet to start. Webshine learning portal! </h1>
+      <h1>Hello {greet}, Project Yet to start. Webshine learning portal!</h1>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad excepturi esse distinctio labore obcaecati suscipit
         repellendus, odio aliquid in voluptates, animi, quae itaque cumque aspernatur provident maxime officiis mollitia
         maiores!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, explicabo nemo hic consectetur porro rem
+        consequatur dolor nesciunt ea. Magni necessitatibus, rerum eveniet est quam culpa enim quo nisi id.
       </p>
     </>
   )
