bafc089c0e7ab05100a75e970e6d0b2cdd1a51d7
Vaishnavan M <145651333+vaishnavansakthi@users.noreply.github.com>
2024-01-28 23:37:27 +0530

    Release/webshinelearning/frontend/fy2024 q1 sprint1 (#26)
    * fixed app.tsx (#24)
    
    Co-authored-by: Priya <padmapriyar@infoservicesllc.com>
    
    * changing app.tsx (#25)
    
    Co-authored-by: vaishnavanm5 <vaishnavanm5@gmail.com>
    
    * fix: updating the code commit flow
    
    * fix: liniting fixs
    
    * Wsl 1 fix/dev test (#27)
    
    * fix: package lock json updates
    
    * fix: test
    
    * test pr template
    
    * adding PR to test the template (#32)
    
    Co-authored-by: vaishnavanm <vaishnavanm5@gmail.com>
    
    * template change (#33)
    
    Co-authored-by: vaishnavanm <vaishnavanm5@gmail.com>
    
    ---------
    
    Co-authored-by: Padma Priya <94052245+Padmapriya1409@users.noreply.github.com>
    Co-authored-by: Priya <padmapriyar@infoservicesllc.com>
    Co-authored-by: vaishnavanm5 <vaishnavanm5@gmail.com>
    Co-authored-by: karthickprojects <125386265+karthickprojects@users.noreply.github.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,53 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    'eslint-config-prettier',
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    sourceType: "module",
+  },
   plugins: ["react-refresh"],
+  settings: {
+    react: {
+      // Tells eslint-plugin-react to automatically detect the version of React to use.
+      version: 'detect',
+    },
+    // Tells eslint how to resolve imports
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+    "@typescript-eslint/no-unused-vars": "off",
+    "no-undef": "off",
+    "import/no-unresolved": 0,
+    "linebreak-style": ["error", "windows"],
+    "indent": "off",
+    "object-curly-spacing": "off",
+    "no-tabs": 0,
+    "max-len": 0,
+    "no-empty": [0, "allow-empty-functions", "allow-empty-catch"],
+    "@typescript-eslint/no-explicit-any": ["off"],
+    "@typescript-eslint/naming-convention": ["off"],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "no-mixed-spaces-and-tabs": 0,
+    "camelcase": 0,
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import "./App.css";
+import React from "react"
+import "./App.css"
 
+/**
+ * Creating App Component
+ * @returns {React.Component} App component
+ */
 function App() {
+  const [greet] = React.useState("Everyone!")
+
   return (
     <>
-      <h1>Hello Everyone, Project Yet to Start for release deployment</h1>
+      <h1>Hello {greet}, Project Yet to start. Webshine learning portal!</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad excepturi esse distinctio labore obcaecati suscipit
+        repellendus, odio aliquid in voluptates, animi, quae itaque cumque aspernatur provident maxime officiis mollitia
+        maiores!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus facere dolorum, fugiat incidunt itaque,
+        saepe repudiandae quis officia dicta iste, assumenda veritatis enim sequi minima neque velit. Voluptates, rem a?
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci modi atque fugit fugiat inventore sint?
+        Quaerat earum ea cupiditate, accusantium amet, est eveniet eligendi sint illo molestiae dolore doloribus!
+        Incidunt!
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
