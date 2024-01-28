b142c01db6685834c18f78dc41316bd8cf54f3b1
vaishnavanm5 <vaishnavanm5@gmail.com>
2024-01-28 10:30:31 +0530

    dev deploy test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Project Yet to Start for release deployment</h1>
+      <h1>Hello Everyone, Project Yet to Start for release deployment</h1>
     </>
   );
 }
