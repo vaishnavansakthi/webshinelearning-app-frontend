07c51741603fb983a8754758f47f2e82d3f29d37
Vaishnavan M <145651333+vaishnavansakthi@users.noreply.github.com>
2024-01-28 12:02:21 +0530

    changing app.tsx (#25)
    Co-authored-by: vaishnavanm5 <vaishnavanm5@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Hello Everyone!, Project Yet to Start for release deployment</h1>
+      <h1>Hello Everyone!, Project Yet to start</h1>
     </>
   );
 }
