1748b52464d311a1ba70487952ac6552ce190471
Vaishnavan M <145651333+vaishnavansakthi@users.noreply.github.com>
2024-02-25 04:27:06 +0530

    feat: updating the story button (#64)

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,31 @@
+import { IButtonProps } from "../../types/button"
+
+const Button = ({ text, color, size, disabled, varient, textColor="text-white" }: IButtonProps) => {
+  return (
+    <>
+      <button
+        className={`
+        ${
+          color === "primary"
+            ? "bg-blue-500 hover:bg-blue-600"
+            : color === "secondary"
+              ? "bg-orange-400 hover:bg-orange-500"
+              : color === "success"
+                ? "bg-green-400 hover:bg-green-500"
+                : color === "error"
+                  ? "bg-red-500 hover:bg-red-600"
+                  : ""
+        }
+         ${varient === "contained" ? "p-4 border-[1px] rounded-md" : varient === "outline" ? "p-4 border-[1px] border-black rounded-md" : ""}
+         ${size === "large" ? "text-lg p-4" : size === "medium" ? "text-md p-2" : "text-sm p-1"}
+         ${textColor} focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-6 py-2.5 text-center md:mr-5 mb-3 md:mb-0 inline-flex items-center justify-center
+        `}
+        disabled={disabled}
+      >
+        {text}
+      </button>
+    </>
+  )
+}
+
+export default Button
