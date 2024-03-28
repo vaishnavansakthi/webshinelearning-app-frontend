00e7421652fed79b51c03ea8f975be93d5a77ec5
Vaishnavan M <145651333+vaishnavansakthi@users.noreply.github.com>
2024-03-28 13:26:09 +0530

    fixed minor issues

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { Link, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { Formik, Form, FormikProps, FormikHelpers } from "formik"
 import { CgDanger } from "react-icons/cg"
 
