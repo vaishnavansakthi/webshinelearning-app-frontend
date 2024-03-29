5a9df168c0f50380b84b6aca2e790c736a0ac888
Vaishnavan M <145651333+vaishnavansakthi@users.noreply.github.com>
2024-03-29 16:56:57 +0530

    test

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,20 +1,26 @@
+import { Helmet } from "react-helmet"
+
 import { DashboardBanner } from "../../components/organisms"
 import { UserCard } from "../../components/moleclues"
+import HomeIcon from "../../assets/icons8-home-64.png"
 
 const Dashboard = () => {
-    
-    return(
-        <>
-            <div className="p-5">
-                <DashboardBanner />
-                <div className="grid grid-cols-12 gap-6">
-                    <UserCard />
-                    <UserCard />
-                    <UserCard />    
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Helmet>
+        <title>Dashboard</title>
+        <link rel="icon" type="image/png" href={HomeIcon} />
+      </Helmet>
+      <div className="p-5">
+        <DashboardBanner />
+        <div className="grid grid-cols-12 gap-6">
+          <UserCard />
+          <UserCard />
+          <UserCard />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Dashboard
