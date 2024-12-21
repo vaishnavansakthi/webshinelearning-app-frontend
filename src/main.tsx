9956b9aebd648926cd4ae8f0989d530533cc54b2
Vaishnavan M <145651333+vaishnavansakthi@users.noreply.github.com>
2024-12-21 14:40:53 +0530

    new google tag manger

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ReactGA from 'react-ga4';
+import TagManager from 'react-gtm-module';
 import App from './App.tsx'
 import './index.css'
 
 const rootElement = document.getElementById('root')
+const tagManagerArgs = {
+  gtmId: 'GTM-WNJDZHGH', // Use your GTM ID
+};
 
 if (!rootElement) {
   throw new Error("Root element with ID 'root' not found in the document.")
@@ -16,6 +20,7 @@ if (!rootElement) {
 ReactGA.initialize('G-V4ZRLH0DM0', {
   // debug: true, // Uncomment this to enable debug mode
 });
+TagManager.initialize(tagManagerArgs);
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
